6c9ff76b2fb98bfb8251f09a0d9fb55aaab26494
FateLiebe <2251061928@e.tlu.edu.vn>
2026-06-28 20:40:29 +0700

    chore: remove temporary python scratch files

--- inspect_boss.py
+++
@@ -1,5 +0,0 @@
-import os
-
-target_path = os.path.abspath(os.path.join(os.getcwd(), "..", "Activity Diagrams", "AD3_BossAI.puml"))
-with open(target_path, "r", encoding="utf-8") as f:
-    print(f.read())
